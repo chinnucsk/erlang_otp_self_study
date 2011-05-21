eb40a0cc01ad3544f61e1d65c6838ed5076b609a
ksauzz <ksauzzmsg@gmail.com>
2011-05-21 15:17:55 +0900

    refactor

--- 20110521/ksauzz/exercise_3_1.erl
+++ 20110521/ksauzz/exercise_3_1.erl
@@ -1,12 +1,9 @@
 -module(exercise_3_1).
 -export([sum/1, sum/2]).
-sum_acc(0,   Total) -> Total;
-sum_acc(Num, Total) -> sum_acc(Num - 1, Total + Num).
+sum_acc([], Total) -> Total;
+sum_acc([Head | Tail], Total) -> sum_acc(Tail, Head + Total).
 
-sum_list_acc([], Total) -> Total;
-sum_list_acc([Head | Tail], Total) -> sum_list_acc(Tail, Head + Total).
-
-sum(Num) -> sum_acc(Num, 0).
+sum(Num) -> sum_acc(lists:seq(1, Num), 0).
 sum(StartNum, EndNum) when StartNum =< EndNum -> 
   List = lists:seq(StartNum, EndNum, 1),
-  sum_list_acc(List, 0).
+  sum_acc(List, 0).
